7de4217bca0ba83555b234d32870141b0e404313
nkramber <natekramber@gmail.com>
2024-12-11 14:03:08 -0600

    Added exception handler

--- distances.py
+++ distances.py
@@ -1,4 +1,27 @@
+distances = {
+    "Voyager 1": "163",
+    "Voyager 2": "136",
+    "Pioneer 10": "80 AU",
+    "New Horizone": "58",
+    "Pioneer 11": "44 AU"
+}
+
+
 def main():
-    print("Hello, world!")
+    spacecraft = input("Enter a spacecraft: ")
+
+    try:
+        au = float(distances[spacecraft])
+    except ValueError:
+        print(f"Can't convert '{distances[spacecraft]}' to a float")
+        return
+
+    m = convert(au)
+    print(f"{m} meters away")
+
+
+def convert(au):
+    return au * 149597870700
+
 
 main()
