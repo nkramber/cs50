a87657659d5a1ce08fd321d769b5877aee9611ef
nkramber <natekramber@gmail.com>
2024-12-12 01:55:59 -0600

    fixed formatting

--- fuel.py
+++ fuel.py
@@ -1,7 +1,7 @@
 def main():
     print(get_percent())
         
-
+        
 def get_percent():
     while True:
         try:
@@ -9,12 +9,14 @@ def get_percent():
             x = int(x)
             y = int(y)
             fuel = int(x / y * 100)
+            
             if fuel < 99 and fuel > 1:
                 return str(fuel) + "%"
             if fuel == 0 or fuel == 1:
                 return "E"
             if fuel == 99 or fuel == 100:
                 return "F"
+            
         except (ValueError, ZeroDivisionError):
             pass
 
